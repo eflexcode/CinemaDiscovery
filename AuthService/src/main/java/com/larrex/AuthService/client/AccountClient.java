d1e966d9e9d0f6b5f8f7f287395af4b0b65e6e6c
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-09-01 02:57:48 +0100

    found

--- AuthService/src/main/java/com/larrex/AuthService/client/AccountClient.java
+++ AuthService/src/main/java/com/larrex/AuthService/client/AccountClient.java
@@ -22,9 +22,4 @@ public interface AccountClient {
     @PostMapping("user/verify/{email}")
     @ResponseStatus(HttpStatus.CREATED)
     ResponseEntity<User> verifyUser(@PathVariable(name = "email") String email);
-
-@PostMapping("user/verify/{email}")
-    @ResponseStatus(HttpStatus.CREATED)
-    ResponseEntity<User> verifyme(@PathVariable(name = "email") String email);
-
 }
